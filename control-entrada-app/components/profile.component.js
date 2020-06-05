98c4b2f725652df3a5ccf30fe3fb4f13f8f050fb
Jose <josekasike@gmail.com>
2020-06-05 17:17:06 -0400

    ajustes en camara

--- control-entrada-app/components/profile.component.js
+++ control-entrada-app/components/profile.component.js
@@ -7,60 +7,51 @@ import {
   Dimensions,
   TouchableOpacity,
   ScrollView,
+  ImageBackground,
+  Image,
 } from "react-native";
+import { Input } from "./input.component";
+import { MainButton } from "./mainButton.component";
+import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 const element1 = () => {
   return (
-    <View>
-      <View>
-        <Text>Jose del Corral</Text>
+    <View style={{ marginTop: 10, paddingHorizontal: 5 }}>
+      <View style={styles.dataBox}>
+        <Text style={styles.labelText}>DNI:</Text>
+        <Text style={styles.dataText}>19222907</Text>
       </View>
-      <View>
-        <Text>19222907</Text>
+      <View style={styles.dataBox}>
+        <Text style={styles.labelText}>Destino:</Text>
+        <Text style={styles.dataText}>Apt 104</Text>
       </View>
-      <View>
-        <Text>Apt 104</Text>
+      <View style={styles.dataBox}>
+        <Text style={styles.labelText}>Hora de Entrada:</Text>
+        <Text style={styles.dataText}>10:00 am</Text>
       </View>
-      <View>
-        <Text>10:00 am</Text>
-      </View>
-      <View>
-        <Text>10:00 pm</Text>
+      <View style={styles.dataBox}>
+        <Text style={styles.labelText}>Hora de Salida:</Text>
+        <Text style={styles.dataText}>10:00 pm</Text>
       </View>
     </View>
   );
 };
 
-const element2 = () => {
-  return (
-    <View style={{ flexDirection: "row" }}>
-      <View>
-        <View>
-          <Text>Jose del Corral</Text>
-        </View>
-        <View>
-          <Text>19222907</Text>
-        </View>
-        <View>
-          <Text>987654</Text>
-        </View>
-      </View>
-      <View>
-        <Text>Foto</Text>
-      </View>
-    </View>
-  );
-};
+
 
 const { width } = Dimensions.get("window");
+const cover = require("../assets/images/background.jpg");
+const profilePic = require("../assets/images/female-2.jpg");
+const watchPic = require("../assets/images/male-2.jpg");
 
 export const ProfileComponent = () => {
-  const [activeTab, setActiveTab] = React.useState('0');
-  
+  const [activeTab, setActiveTab] = React.useState("0");
 
   const [xTabOne, setXTabOne] = React.useState();
   const [xTabTwo, setXTabTwo] = React.useState();
-  
+  const [hightContent1, setHightContent1] = React.useState(-5000);
+  const [saveImg, setSaveImg] = React.useState()
   const translateTab = new Animated.Value(0);
   const translateContent1 = new Animated.Value(0);
   const translateContent2 = new Animated.Value(width);
@@ -74,7 +65,6 @@ export const ProfileComponent = () => {
       bounciness: 5,
     }).start();
     if (tabActive == 0) {
-      
       Animated.parallel([
         Animated.spring(translateContent1, {
           toValue: 0,
@@ -84,14 +74,13 @@ export const ProfileComponent = () => {
           toValue: width,
           duration: 500,
           speed: 12,
-          
+
           bounciness: 5,
         }).start(),
       ]);
       //setActiveTab1({color: 'orange'})
       //setActiveTab2({color: 'grey'})
     } else {
-      
       Animated.parallel([
         Animated.spring(translateContent1, {
           toValue: -width,
@@ -101,7 +90,7 @@ export const ProfileComponent = () => {
           toValue: 0,
           duration: 500,
           speed: 12,
-          
+
           bounciness: 5,
         }).start(),
       ]);
@@ -110,27 +99,80 @@ export const ProfileComponent = () => {
     }
   };
 
-  return (
-    <View style={{ flex: 1 }}>
+  const element2 = () => {
+    return (
       <View
         style={{
-          flex: 1,
-          backgroundColor: "teal",
-          justifyContent: "center",
-          alignItems: "center",
+          paddingHorizontal: 5,
+          marginTop: 10,
+          flexDirection: "row",
+          justifyContent: "space-between",
         }}
       >
-        <Text>Foto de Perfil</Text>
+        <View>
+          <View style={styles.dataBox}>
+            <Text style={styles.labelText}>Nombre:</Text>
+            <Text style={styles.dataText}>Jose Del Corral</Text>
+          </View>
+          <View style={styles.dataBox}>
+            <Text style={styles.labelText}>DNI</Text>
+            <Text style={styles.dataText}>19222907</Text>
+          </View>
+          <View style={styles.dataBox}>
+            <Text style={styles.labelText}>Identificaion:</Text>
+            <Text style={styles.dataText}>9987654</Text>
+          </View>
+        </View>
+        <View style={{ justifyContent: "center" }}>
+          <Image
+            source={watchPic}
+            style={{ width: 140, height: 140, borderRadius: 70 }}
+          />
+          <Image
+            source={{uri: saveImg}}
+            style={{ width: 120, height: 120, borderRadius: 70 }}
+          />
+        </View>
       </View>
+    );
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync();
+
+    console.log(result);
+
+    setSaveImg(result.uri);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={cover} style={styles.imgBackground}>
+        <View style={styles.cover}>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {/* <Ionicons name="ios-person" size={120} color="#fff" /> */}
+            <View style={{ position: "relative", marginBottom: 10 }}>
+              <Image source={profilePic} style={styles.profilePic} />
+              <TouchableOpacity
+                onPress={() => pickImage(res => {
+                  console.log(res)
+                })}
+                style={styles.cameraIcon}
+              >
+                <Ionicons name="ios-camera" size={38} color="orange" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.nameBox}>
+              <Text style={styles.nameText}>Jose Del Corral</Text>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
       <View style={{ flex: 1 }}>
-        <View style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
-          <View
-            style={{
-              flexDirection: "row",
-              height: 50,
-              position: "relative",
-            }}
-          >
+        <View style={{ paddingHorizontal: 5 }}>
+          {/* //------CODE FROM TABBAR------// */}
+          {/* ///----------------------------//////// */}
+          <View style={styles.tabBar}>
             <Animated.View
               style={{
                 //backgroundColor: "blue",
@@ -149,13 +191,7 @@ export const ProfileComponent = () => {
               }}
             />
             <TouchableOpacity
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                //borderBottomWidth: 1,
-                borderColor: "grey",
-              }}
+              style={styles.tab1}
               onLayout={(event) => {
                 setXTabOne(event.nativeEvent.layout.x);
               }}
@@ -163,18 +199,12 @@ export const ProfileComponent = () => {
                 animatedOverlay(xTabOne, 0);
               }}
             >
-              <Text style={{ color: activeTab === '0' ? 'orange' : 'grey' }}>
-                Tab One
+              <Text style={{ color: activeTab === "0" ? "orange" : "grey" }}>
+                Datos
               </Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                //borderBottomWidth: 1,
-                borderColor: "grey",
-              }}
+              style={styles.tab2}
               onLayout={(event) => {
                 setXTabTwo(event.nativeEvent.layout.x);
               }}
@@ -182,13 +212,16 @@ export const ProfileComponent = () => {
                 animatedOverlay(xTabTwo, 1);
               }}
             >
-              <Text style={{ color: activeTab === '1' ? 'orange' : 'grey'}}>
-                Tab Two
+              <Text style={{ color: activeTab === "1" ? "orange" : "grey" }}>
+                Seguridad
               </Text>
             </TouchableOpacity>
           </View>
-          <ScrollView horizontal={true}>
+          <View>
             <Animated.View
+              onLayout={(event) => {
+                setHightContent1(event.nativeEvent.layout.height);
+              }}
               style={{
                 transform: [
                   {
@@ -205,14 +238,85 @@ export const ProfileComponent = () => {
                   {
                     translateX: translateContent2,
                   },
+                  {
+                    translateY: -hightContent1,
+                  },
                 ],
               }}
             >
               {element2()}
             </Animated.View>
-          </ScrollView>
+          </View>
         </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  cover: {
+    backgroundColor: "black",
+    flex: 1,
+    alignItems: "center",
+    opacity: 0.8,
+    justifyContent: "center",
+  },
+  profilePic: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+  },
+  cameraIcon: {
+    position: "absolute",
+    bottom: 0,
+    right: 5,
+  },
+  nameBox: {
+    height: 40,
+    backgroundColor: "orange",
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    top: "10%",
+    justifyContent: "center",
+  },
+  nameText: {
+    textAlign: "center",
+    fontSize: 17,
+    color: "#fff",
+  },
+  tabBar: {
+    flexDirection: "row",
+    height: 50,
+    position: "relative",
+  },
+  tab1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    //borderBottomWidth: 1,
+    borderColor: "grey",
+  },
+  tab2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    //borderBottomWidth: 1,
+    borderColor: "grey",
+  },
+  //elemento 1
+  dataBox: {
+    marginVertical: 10,
+  },
+  labelText: {
+    fontSize: 14,
+  },
+  dataText: {
+    fontSize: 17,
+  },
+  //emento 2
+});
