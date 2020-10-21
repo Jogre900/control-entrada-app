46abdc2f4852671ca012da9e89cc75fbc5d387ff
Jose Segovia <jose.segovia.r@gmail.com>
2020-10-21 15:24:43 -0400

    send props from navigator

--- control-entrada-app/navigation/main.navigator.jsx
+++ control-entrada-app/navigation/main.navigator.jsx
@@ -3,19 +3,19 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 //screens
 import { SplashScreen } from "../components/splashScreen.component";
-import { ProfileComponent } from '../components/profile.component'
+import { ProfileComponent } from "../components/profile.component";
 import { LogInScreen } from "../screens/main/logInScreen";
 import { WatchNavigator } from "./watch.navigator";
 import { SuperNavigator } from "./supervicer.navigator";
 
 const stack = createStackNavigator();
 
-export const MainNavigator = () => {
+export const MainNavigator = (props) => {
   return (
     <stack.Navigator headerMode="none">
       {/* <stack.Screen name="splash" component={ProfileComponent} /> */}
       <stack.Screen name="logIn" component={LogInScreen} />
-      <stack.Screen name="watch" component={WatchNavigator} />
+      <stack.Screen name="watch">{() => <WatchNavigator {...props} />}</stack.Screen>
       <stack.Screen name="super" component={SuperNavigator} />
     </stack.Navigator>
   );
