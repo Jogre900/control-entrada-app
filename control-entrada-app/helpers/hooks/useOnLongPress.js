eebfa127f10ff199bf9bef50f7b7fd2f5b4b2ad2
Jose del Corral <josekasike@gmail.com>
2021-02-17 12:07:04 -0400

    ajustando onLong hook

---
+++ control-entrada-app/helpers/hooks/useOnLongPress.js
@@ -0,0 +1,24 @@
+import {useState, useEffect} from 'react'
+import { Vibration } from 'react-native'
+
+export const useOnLongPress = ({id}) => {
+    console.log("HOOKS------",id)
+    const [selectItem, setSeletedItem] = useState([]);
+  const [changeStyle, setChangeStyle] = useState(false);
+    
+  const onLong = () => {
+    if (selectItem.includes(id)) {
+      setSeletedItem((value) => value.filter((elem) => elem !== id));
+      //hideCheckMark();
+      return;
+    }
+    Vibration.vibrate(100),
+      setSeletedItem(selectItem.concat(id)),
+      //showCheckMark();
+    setChangeStyle(!changeStyle);
+  };
+  useEffect(() => {
+      onLong()
+  }, [id])
+  return {selectItem, setSeletedItem}
+}
